146105dabaf2326b0008585f1d08a506848c6bad
lontten <lontten@163.com>
2025-03-10 20:25:56 +0800

    调整电话校验

--- lcutils/phone.go
+++ lcutils/phone.go
@@ -6,18 +6,14 @@ import (
 
 func CheckPhoneAll(phoneNumber string) bool {
 	// 手机号
-	pattern := "^1[3-9][0-9]{9}$"
-	regex := regexp.MustCompile(pattern)
-	matched := regex.MatchString(phoneNumber)
-	if matched {
+	phone := CheckPhone(phoneNumber)
+	if phone {
 		return true
 	}
 
-	// 固话 区号（2-3位） + 连字符 + 本地号码（7-8位） + 连字符 + 分机号（2-4位）
-	pattern = `^0\d{2,3}-\d{7,8}(-\d{2,4})?$`
-	regex = regexp.MustCompile(pattern)
-	matched = regex.MatchString(phoneNumber)
-	if matched {
+	//广义范围的电话号码
+	hyphen := isValidNumberWithHyphen(phoneNumber)
+	if hyphen {
 		return true
 	}
 	return false
@@ -43,4 +39,15 @@ func CheckLandline(phoneNumber string) bool {
 		return true
 	}
 	return false
+}
+
+// 判断是否符合数字加横杠格式
+func isValidNumberWithHyphen(s string) bool {
+	// 长度在4到30位之间
+	if len(s) < 4 || len(s) > 22 {
+		return false
+	}
+	// 只包含数字和横杠，且横杠不能连续出现，也不能出现在开头或结尾
+	matched, _ := regexp.MatchString(`^\d+(?:-\d+)*$`, s)
+	return matched
 }
