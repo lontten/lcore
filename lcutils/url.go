9347e6f9c6737cfe63752fc5d24fb3a53d4d9714
lontten <lontten@163.com>
2025-10-29 17:53:18 +0800

    SanitizeFileName4URL 默认截取15位

--- lcutils/url.go
+++ lcutils/url.go
@@ -11,7 +11,7 @@ var reservedChars = map[rune]struct{}{
 	')': {}, '*': {}, '+': {}, ',': {}, ';': {}, '=': {},
 }
 
-func SanitizeFileName4URL(url string) string {
+func SanitizeFileName4URL(url string, size ...int) string {
 	var sb strings.Builder
 	sb.Grow(len(url)) // 预分配内存提高效率
 
@@ -23,5 +23,17 @@ func SanitizeFileName4URL(url string) string {
 		}
 	}
 
-	return sb.String()
+	s := sb.String()
+	l := 15
+	if len(size) > 0 {
+		l = size[0]
+	}
+
+	runes := []rune(s)
+	// 如果字符总数小于等于15，直接返回原字符串
+	if len(runes) <= l {
+		return s
+	}
+	// 否则返回前15个字符
+	return string(runes[:l])
 }
