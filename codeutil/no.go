ab5557cb36977b0796659ab1e57817429cf6630f
lontten <lontten@163.com>
2025-11-07 10:27:10 +0800

    💥 随机生成，重构

--- codeutil/no.go
+++ codeutil/no.go
@@ -1,62 +1,38 @@
 package codeutil
 
 import (
-	"crypto/rand"
 	"time"
 )
 
-// NewTimedRandomID32
+// RandomTimeID32
 // 格式 yyyymmddHHMMSS + 18位随机数,有字母
-func NewTimedRandomID32() string {
-	return NewTimedRandomID(32)
+func RandomTimeID32() string {
+	return RandomTimeID(32)
 }
 
-// NewTimedRandomID
+// RandomTimeID
 // 格式 yyyymmddHHMMSS + n位随机数,有字母
-func NewTimedRandomID(length int) string {
-	const charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	timestamp := time.Now().Format("20060102150405") // 14字符
-
-	randomLength := length - len(timestamp)
-	if randomLength <= 0 {
-		return timestamp[:length]
-	}
-
-	randomBytes := make([]byte, randomLength)
-	if _, err := rand.Read(randomBytes); err != nil {
-		panic("failed to generate random ID: " + err.Error())
-	}
-
-	for i := range randomBytes {
-		randomBytes[i] = charset[randomBytes[i]%byte(len(charset))]
-	}
-	return timestamp + string(randomBytes)
+func RandomTimeID(length int) string {
+	return RandomTimeBaseID(UpperNumCharset, length)
 }
 
-// NewTimedRandomNumberID32
+// RandomTimeNumberID32
 // 格式 yyyymmddHHMMSS + 18位随机数字
-func NewTimedRandomNumberID32() string {
-	return NewTimedRandomNumberID(32)
+func RandomTimeNumberID32() string {
+	return RandomTimeNumberID(32)
 }
 
-// NewTimedRandomNumberID
+// RandomTimeNumberID
 // 格式 yyyymmddHHMMSS + n位随机数字
-func NewTimedRandomNumberID(length int) string {
-	const charset = "0123456789"                     // 仅数字
-	timestamp := time.Now().Format("20060102150405") // 14字符
+func RandomTimeNumberID(length int) string {
+	return RandomTimeBaseID(DigitCharset, length)
+}
 
+func RandomTimeBaseID(charset string, length int) string {
+	timestamp := time.Now().Format("20060102150405") // 14字符
 	randomLength := length - len(timestamp)
 	if randomLength <= 0 {
 		return timestamp[:length]
 	}
-
-	randomBytes := make([]byte, randomLength)
-	if _, err := rand.Read(randomBytes); err != nil {
-		panic("failed to generate random ID: " + err.Error())
-	}
-
-	for i := range randomBytes {
-		randomBytes[i] = charset[randomBytes[i]%byte(len(charset))]
-	}
-	return timestamp + string(randomBytes)
+	return timestamp + RandomStrFromCharset(charset, randomLength)
 }
