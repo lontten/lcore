59f5ecfef4da87f28a1ec929ee58460934842ec7
lontten <lontten@163.com>
2025-11-13 15:07:04 +0800

    fix 优化Struct2StringMap 方法

--- structutil/map.go
+++ structutil/map.go
@@ -1,14 +1,51 @@
 package structutil
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
-func Struct2StringMap(s any) map[string]string {
+func Struct2StringMap(s any) (map[string]string, error) {
 	val := reflect.ValueOf(s)
+
+	// 解引用指针（支持结构体指针输入）
+	for val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	// 校验输入是否为结构体
+	if val.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("input is not a struct (got %s)", val.Kind())
+	}
+
 	relType := val.Type()
 	numField := relType.NumField()
 	m := make(map[string]string, numField)
+
 	for i := 0; i < numField; i++ {
-		m[relType.Field(i).Name] = val.Field(i).String()
+		field := relType.Field(i)
+		fieldVal := val.Field(i)
+
+		// 转换字段值为字符串（支持多种类型）
+		var strVal string
+		switch fieldVal.Kind() {
+		case reflect.String:
+			strVal = fieldVal.String()
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			strVal = fmt.Sprintf("%d", fieldVal.Int())
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			strVal = fmt.Sprintf("%d", fieldVal.Uint())
+		case reflect.Float32, reflect.Float64:
+			strVal = fmt.Sprintf("%v", fieldVal.Float())
+		case reflect.Bool:
+			strVal = fmt.Sprintf("%t", fieldVal.Bool())
+		default:
+			// 处理其他未覆盖的类型（如切片、嵌套结构体等，可根据需求扩展）
+			strVal = fmt.Sprintf("%v", fieldVal.Interface())
+		}
+
+		m[field.Name] = strVal
 	}
-	return m
+
+	return m, nil
 }
